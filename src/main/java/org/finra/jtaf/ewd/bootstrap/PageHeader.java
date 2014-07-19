a64cb8dbd222d24de183d12d0ed8a19dd49fef2e
Tim Ski <marshyski@gmail.com>
2014-07-19 16:26:07 -0400

    Added comments to PageHeader and removed print test

--- src/main/java/org/finra/jtaf/ewd/bootstrap/PageHeader.java
+++ src/main/java/org/finra/jtaf/ewd/bootstrap/PageHeader.java
@@ -3,24 +3,15 @@ package org.finra.jtaf.ewd.bootstrap;
 import org.finra.jtaf.ewd.widget.WidgetException;
 import org.finra.jtaf.ewd.widget.element.Element;
 
-/**
- * Created with IntelliJ IDEA.
- * User: marcinot
- * Date: 7/19/14
- * Time: 2:20 PM
- * To change this template use File | Settings | File Templates.
- */
-
 public class PageHeader extends Element {
     public PageHeader(String locator) {
         super(locator);
     }
 
+    /**
+    Get page header string info http://getbootstrap.com/components/#page-header
+    */
     public String getHeader(String type) throws WidgetException {
         return new Element(getLocator() + "//" + type).getText();
-    }
-
-    public String testMethod() {
-        return "hello";
     }
 }
